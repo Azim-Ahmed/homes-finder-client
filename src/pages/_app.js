777e964023ef08c2f4b8c33c4a82e8d7943e4e46
Azim-Ahmed <azimuahmed13@gmail.com>
2021-03-21 00:32:11 +0600

    parallax solved

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,6 @@ import { createContext, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
 import Head from 'next/head';
-import { ParallaxProvider } from 'react-scroll-parallax';
 
 export const UserContext = createContext();
 
@@ -16,12 +15,10 @@ function MyApp({ Component, pageProps }) {
     <UserContext.Provider
       value={[appData, setAppData, loggedIn, setLoggedIn, admin, setAdmin]}
     >
-      <ParallaxProvider>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </Head>
-        <Component {...pageProps} />
-      </ParallaxProvider>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
     </UserContext.Provider>
   );
 }
